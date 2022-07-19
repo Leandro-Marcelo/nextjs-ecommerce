78b4f396e17be8992aefd07ccf4f87c193c0e72b
LeandroMarcelo <lmarcelos2019@gmail.com>
2022-07-19 16:14:46 -0400

    deploy6

--- pages/api/products.js
+++ pages/api/products.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
             id: 0,
             title: "Figurita octocat",
             price: 100,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/octocat.webp",
+            image: `${process.env.VERCEL_URL}/img/octocat.webp`,
             description:
                 "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia maiores praesentium vitae sequi consectetur itaque ea inventore architecto nemo. Enim id perspiciatis minus modi qui at commodi quos quisquam similique!",
         },
@@ -16,7 +16,7 @@ export default function handler(req, res) {
             title: "Gorra",
             price: 200,
             // ? Estoy haciendo una solicitud http a localhost:3000/img/octocat.webp, ya que si utilizara otro servidor web para almacenar imagenes u otro servicio en la nube, aquí tendría que colocar toda la dirección. Como acá estoy simulando una API, no puedo utilizar una dirección relativa a mi proyecto, tengo que usar toda la URL absoluta porque de esa forma voy a utilizar otro componente que tiene NextJS para poder renderizar estas imagenes.
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/mug.webp",
+            image: `${process.env.VERCEL_URL}/img/mug.webp`,
             description:
                 "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia maiores praesentium vitae sequi consectetur itaque ea inventore architecto nemo. Enim id perspiciatis minus modi qui at commodi quos quisquam similique!",
         },
@@ -24,7 +24,7 @@ export default function handler(req, res) {
             id: 2,
             title: "Sudadera Halo",
             price: 200,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/tshirt01.webp",
+            image: `${process.env.VERCEL_URL}/img/tshirt01.webp`,
             description:
                 "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia maiores praesentium vitae sequi consectetur itaque ea inventore architecto nemo. Enim id perspiciatis minus modi qui at commodi quos quisquam similique!",
         },
@@ -32,7 +32,7 @@ export default function handler(req, res) {
             id: 3,
             title: "Introvert T-shirt",
             price: 200,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/tshirt02.webp",
+            image: `${process.env.VERCEL_URL}/img/tshirt02.webp`,
             description:
                 "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia maiores praesentium vitae sequi consectetur itaque ea inventore architecto nemo. Enim id perspiciatis minus modi qui at commodi quos quisquam similique!",
         },
@@ -40,7 +40,7 @@ export default function handler(req, res) {
             id: 4,
             title: "Invertocat 2.0 Coasters",
             price: 30,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/coaster01.webp",
+            image: `${process.env.VERCEL_URL}/img/coaster01.webp`,
             description:
                 "Give your desk nothing but the best and upgrade your coaster game with these all new environmentally-friendly bamboo Invertocat 2.0 Coasters. Includes four laser-etched coasters with coaster stand.",
         },
@@ -48,7 +48,7 @@ export default function handler(req, res) {
             id: 5,
             title: "Glow in the Dark Sticker Pack",
             price: 5,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/stickers02.webp",
+            image: `${process.env.VERCEL_URL}/img/stickers02.webp`,
             description:
                 "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia maiores praesentium vitae sequi consectetur itaque ea inventore architecto nemo. Enim id perspiciatis minus modi qui at commodi quos quisquam similique!",
         },
@@ -56,7 +56,7 @@ export default function handler(req, res) {
             id: 6,
             title: "Adventure Sticker Pack",
             price: 10,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/stickers01.webp",
+            image: `${process.env.VERCEL_URL}/img/stickers01.webp`,
             description:
                 "We’re going on an adventure! Let your imagination wander with the new Adventure  Sticker Pack. With several never before printed Octocats and some old friends, this pack is here to celebrate the journey as much as the end.",
         },
@@ -64,7 +64,7 @@ export default function handler(req, res) {
             id: 7,
             title: "Invertocat Pullover Hoodie",
             price: 10,
-            image: "nextjs-ecommerce-682dbwejh-leandro-marcelo.vercel.app/img/pullover01.webp",
+            image: `${process.env.VERCEL_URL}/img/pullover01.webp`,
             description:
                 "While there’s nothing like an original black GitHub hoodie, these new pullover hoodies are an instant classic. Featuring tone-on-tone embroidered Invertocats on the chest and wrist, these understated hoodies will have you looking so fresh and so clean (and so warm!).",
         },
