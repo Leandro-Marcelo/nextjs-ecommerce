18431f17c6a200a790cf7c963ab515d47871f247
LeandroMarcelo <lmarcelos2019@gmail.com>
2022-07-19 16:08:56 -0400

    deploy 4

--- services/storeService.js
+++ services/storeService.js
@@ -2,6 +2,7 @@
 // * Ahora bien, si tuviera un backend, un servidor web o una API, aquí mandaríamos a llamar a nuestros datos
 
 export async function getProducts() {
+    console.log(`${process.env.VERCEL_URL}`);
     const request = await fetch(`${process.env.VERCEL_URL}/api/products`);
     const products = await request.json();
 
